759dc1dbefd4cde04a3c8901501ae39ead4a3835
unfor9otten <unfor9otten@gmail.com>
2018-12-09 22:49:03 +0900

    /Users/unfor9otten/blog/conf.py update

--- conf.py
+++ conf.py
@@ -959,12 +959,12 @@ RSS_COPYRIGHT_FORMATS = CONTENT_FOOTER_FORMATS
 # systems.  The following comment systems are supported by Nikola:
 #   disqus, facebook, intensedebate, isso, livefyre, muut
 # You can leave this option blank to disable comments.
-COMMENT_SYSTEM = "muut"
+COMMENT_SYSTEM = "disqus"
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "test"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
