33a77d636dd64bd9b4bc84406a0bc948eec80c4a
unfor9otten <unfor9otten@gmail.com>
2018-12-09 23:35:26 +0900

    /Users/unfor9otten/blog/conf.py update

--- conf.py
+++ conf.py
@@ -964,7 +964,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "unfor9otten"
+COMMENT_SYSTEM_ID = "uf9t"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
